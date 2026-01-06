b5d0f5e82652602b064d0fde2bbee4b8b0e87422
Rabbi <rabbi2206d@aptechgd.net>
2026-01-06 20:59:20 +0500

    step2

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,5 +4,6 @@ from .controllers import NotificationsController
 app_name = 'notifications'
 
 urlpatterns = [
+    path('', NotificationsController.notifications_view, name='notifications'),
     path('mark-read/', NotificationsController.mark_notifications_read_view, name='mark_read'),
 ]
